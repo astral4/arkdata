8eab98cbea040f420f8821451881cde9d1b247ab
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-17 13:20:09 -0500

    more lifetimes + filter out unchanged assets

--- src/assets.rs
+++ src/assets.rs
@@ -1,64 +1,48 @@
 use ahash::HashMap;
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 use std::{fs::File, io::BufReader};
 
 #[derive(Serialize, Deserialize)]
-struct NameHashMapping {
+pub struct NameHashMapping {
     #[serde(flatten)]
-    map: HashMap<String, String>,
+    pub map: HashMap<String, String>,
 }
 
 impl NameHashMapping {
-    fn get(path: &str) -> Self {
+    #[must_use]
+    pub fn get(path: &str) -> Self {
         let file = File::open(path).expect("Failed to open name-to-hash file");
         serde_json::from_reader(BufReader::new(file))
             .expect("Failed to deserialize name-to-hash data")
     }
 }
 
-impl From<Vec<AssetData>> for NameHashMapping {
-    fn from(value: Vec<AssetData>) -> Self {
+impl<'a> From<Vec<AssetData<'a>>> for NameHashMapping {
+    fn from(_value: Vec<AssetData>) -> Self {
         todo!()
     }
 }
 
 #[derive(Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// #[serde(deny_unknown_fields)]
-pub struct AssetData {
-    name: String,
-    // hash: String,
-    md5: String,
-    // pid: Option<String>,
-    // #[serde(rename = "type")]
-    // asset_type: Option<String>,
-    // total_size: u32,
-    // ab_size: u32,
-    // cid: u32,
+pub struct AssetData<'a> {
+    pub name: Cow<'a, String>,
+    pub md5: String,
 }
 
 #[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
-// #[serde(deny_unknown_fields)]
-pub struct UpdateInfo {
-    // full_pack: FullPack,
-    // version_id: String,
-    pub ab_infos: Vec<AssetData>,
-    // count_of_typed_res: u32,
-    // pack_infos: Vec<AssetData>,
+pub struct UpdateInfo<'a> {
+    pub ab_infos: Vec<AssetData<'a>>,
 }
 
-pub fn bar() {
-    let baz = HashMap::<String, String>::default();
-}
-
-impl UpdateInfo {
+impl UpdateInfo<'_> {
     /// # Errors
     /// Returns Err if the HTTP response fetching fails in some way.
     pub async fn fetch_latest(
         client: &reqwest::Client,
         url: String,
-    ) -> Result<Self, reqwest::Error> {
+    ) -> Result<UpdateInfo<'_>, reqwest::Error> {
         let response = client
             .get(url)
             .send()
@@ -66,7 +50,7 @@ impl UpdateInfo {
             .error_for_status()?
             .text()
             .await?;
-        let update_info: Self =
+        let update_info: UpdateInfo<'_> =
             serde_json::from_str(response.as_str()).expect("Failed to read response as UpdateInfo");
         Ok(update_info)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
 
-use arkdata::{Details, UpdateInfo, Version, BASE_URL};
+use arkdata::{Details, NameHashMapping, UpdateInfo, Version, BASE_URL};
 use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
     let mut details = Details::get("details.json");
+    let mut name_to_hash_mapping = NameHashMapping::get("hashes.json").map;
     let client = Client::new();
 
     let data_version = Version::fetch_latest(&client, format!("{BASE_URL}/version"))
@@ -26,6 +27,18 @@ async fn main() {
     .await
     .expect("Failed to fetch asset info list")
     .ab_infos;
+
+    let mut new_assets = Vec::<String>::with_capacity(asset_info.len());
+
+    for entry in asset_info {
+        if name_to_hash_mapping
+            .get(entry.name.as_ref())
+            .map_or(true, |hash| hash != &entry.md5)
+        {
+            name_to_hash_mapping.insert(entry.name.to_string(), entry.md5);
+            new_assets.push(entry.name.into_owned());
+        }
+    }
 
     details.save();
 }
