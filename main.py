197fa484f8807575b8fcce27b31678cabd45032c
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-29 23:33:58 -0500

    use config.toml for Python program

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import tomllib
 import kawapack
 
 if __name__ == "__main__":
-    kawapack.convert("assets", "results")
+    with open("config.toml", mode="rb") as f:
+        config = tomllib.load(f)["fetch-settings"]
+
+    kawapack.convert(config["input_dir"], config["output_dir"])
