c7b8deff0bc78fa6ee9b84b44dec0c2d181328d1
astral4 <88992929+astral4@users.noreply.github.com>
2023-01-14 15:36:58 -0500

    update kawapack

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tomllib
+from pathlib import Path
 import kawapack
 
 if __name__ == "__main__":
     with open("config.toml", mode="rb") as f:
         config = tomllib.load(f)["unpack-settings"]
 
-    kawapack.convert(
-        config["input_dir"],
+    input_dir = config["input_dir"]
+    portrait_dir = Path(input_dir, "arts", "charportraits")
+
+    kawapack.extract_all(
+        input_dir,
         config["output_dir"],
         config.get("match_patterns"),
-        show_logs=False
+        portrait_dir=portrait_dir
     )
