93acca839b96d254114afbc63ac0ea777b0b75ed
astral4 <88992929+astral4@users.noreply.github.com>
2023-01-14 19:51:33 -0500

    add option to supply server as env var

--- src/settings.rs
+++ src/settings.rs
@@ -1,12 +1,23 @@
 use config::{Config, File, FileFormat};
 use once_cell::sync::Lazy;
 use serde::Deserialize;
-use std::path::PathBuf;
+use std::{env, path::PathBuf, str::FromStr};
 
 #[derive(Deserialize)]
 enum Server {
     US,
     CN,
+}
+
+impl FromStr for Server {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "US" => Ok(Self::US),
+            "CN" => Ok(Self::CN),
+            _ => Err(()),
+        }
+    }
 }
 
 #[derive(Default)]
@@ -17,7 +28,7 @@ pub struct ServerLink {
 
 #[derive(Deserialize)]
 pub struct Settings {
-    server: Server,
+    server: Option<Server>,
     #[serde(skip)]
     pub server_url: ServerLink,
     pub details_path: String,
@@ -46,22 +57,34 @@ pub static CONFIG: Lazy<Settings> = Lazy::new(|| {
         .expect("Failed to deserialize configuration file")
         .fetch_settings;
 
-    settings.server_url = match settings.server {
-        Server::US => ServerLink {
-            version: String::from(
-                "https://ark-us-static-online.yo-star.com/assetbundle/official/Android/version",
-            ),
-            base: String::from(
-                "https://ark-us-static-online.yo-star.com/assetbundle/official/Android",
-            ),
-        },
-        Server::CN => ServerLink {
-            version: String::from(
-                "https://ak-conf.hypergryph.com/config/prod/official/Android/version",
-            ),
-            base: String::from("https://ak.hycdn.cn/assetbundle/official/Android"),
-        },
-    };
+    // If a server is supplied as an environmental variable,
+    // it takes precedence over the server specified in config.toml
+    if let Some(server) = env::args().nth(1) {
+        settings.server = Some(
+            Server::from_str(&server).expect("Failed to parse server from environment variable"),
+        )
+    }
+
+    if let Some(server) = &settings.server {
+        settings.server_url = match server {
+            Server::US => ServerLink {
+                version: String::from(
+                    "https://ark-us-static-online.yo-star.com/assetbundle/official/Android/version",
+                ),
+                base: String::from(
+                    "https://ark-us-static-online.yo-star.com/assetbundle/official/Android",
+                ),
+            },
+            Server::CN => ServerLink {
+                version: String::from(
+                    "https://ak-conf.hypergryph.com/config/prod/official/Android/version",
+                ),
+                base: String::from("https://ak.hycdn.cn/assetbundle/official/Android"),
+            },
+        }
+    } else {
+        panic!("No server was provided")
+    }
 
     settings.output_path = PathBuf::from(&settings.output_dir);
 
